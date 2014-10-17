85ec73092770ad4667cf69000cc34e83cfc52384
PluginLearner <itsminecrafttime32@gmail.com>
2014-10-17 14:21:46 -0700

    Sky

---
+++ SkyLegends.js
@@ -0,0 +1,34 @@
+
+ModPE.setItem(400, "skull_creeper", 0, "Wood Crook");
+
+ModPE.setItem(401, "skull_skeleton", 1, "Stone Crook");
+
+ModPE.setItem(402, "skull_zombie", 2, "Iron Crook");
+
+ModPE.setItem(403, "skull_wither", 3, "Gold Crook");
+
+ModPe.setItem(404, "skull_steve", 4, "Diamond Crook);
+
+Item.addShapedRecipe(400, 1, 0, [
+"ss "
+" s "
+"  s"
+],
+
+["s", 280, 0];
+
+Item.addShapedRecipe(401, 1, 0, [
+"ts "
+" s "
+"  s"
+],
+
+["t", 4, 0];
+
+Item.addShapedRecipe(403, 1, 0, [
+"is "
+" s "
+"  s"
+]
+
+["i", 265, 0];
